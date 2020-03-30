7ded691226436ed742dc2e5e811db41fc66b693a
Michał <michal.pasierbiewicz.314@gmail.com>
2020-03-30 20:18:23 +0200

    Add app

---
+++ testmaven/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Infant mix = new Infant();
+
+        System.out.println("Przed zmianami: Od czasu gdy Jarząbek żyje z Hermenegildą zdrowie jego mamusi uległo znacznej poprawie, dzięki czemu nie musi już kupować drogich i trujących leków.");
+        System.out.println("Po zmianach: " + mix.put_changes("Od czasu gdy Jarząbek żyje z Hermenegildą zdrowie jego mamusi uległo znacznej poprawie, dzięki czemu nie musi już kupować drogich i trujących leków."));
+    }
+}
